3dbdd350afc0783c74aca3c03f9a4c6313be0bce
Davide Bottone <d.bottone@pm.me>
2026-02-09 20:28:53 +0000

    Changed upload script to absolute paths

--- upload_runs.sh
+++ upload_runs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # Upload runs to remote storage
-mkdir data/runs/
+mkdir /home/renku/work/data/runs/
 rsync -avz --progress /home/renku/work/data-local/runs/ /home/renku/work/data/runs/
